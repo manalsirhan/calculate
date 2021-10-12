9daed96a41cc5a9ec68f40820c6790799b91baa1
manalsirhan <78295723+manalsirhan@users.noreply.github.com>
2021-10-12 23:19:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
-let numbers = document.getElementById("numbers");
-let number = document.getElementsByClassName("number");
-console.log(number);
-number=[...number]
+const numbers = document.querySelector("#numbers");
+const number = document.querySelectorAll(".number");
+const math = document.querySelectorAll(".math");
+const equal = document.querySelector("#equal");
+const finalRes = document.querySelector("#finalRes");
+
+let num = "";
+let num1 = "";
+let mathCalc="";
+
 number.forEach(element => {
     element.addEventListener('click',()=>{
-        console.log(element.id);
+        num+=element.id;
+        finalRes.innerText=num;
+        console.log("num",num);
     })
 });
+
+math.forEach(element => {
+    element.addEventListener('click',()=>{
+        num1=num;
+        num=""
+        mathCalc=element.id;
+        console.log(mathCalc);
+    })
+});
+
+equal.addEventListener('click',()=>{
+        num= parseFloat(num) ;
+        num1= parseFloat(num1);
+        console.log(num1,num );
+        calculation(num, num1);
+       
+})
+
+function calculation(a , b) {
+ 
+    switch (mathCalc) {
+        case "plus": finalRes.innerText= a+b ;
+            break;
+        case "minus": finalRes.innerText= b-a;
+            break;
+        case "multiplication": finalRes.innerText= b*a;
+            break;
+        case "part": finalRes.innerText= b/a;
+            break;
+        case "pwr": finalRes.innerText= Math.pow(b,a);
+            break;
+    
+        default: console.log("error");
+            break;
+    }
+    num="";
+    num1="";
+    mathCalc="";
+}
